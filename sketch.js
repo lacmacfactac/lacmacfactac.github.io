96eefbb858219fa928b852f9b9652f2ca596e405
lacmacfactac <andras.laszlo.halak@gmail.com>
2018-04-22 00:37:33 +0200

    test

--- sketch.js
+++ sketch.js
@@ -19,6 +19,11 @@ var menuHeight = 80;
 var dimLevel = 0;
 var parentDiv;
 
+var fishLimit = 200;
+
+var isMobile = false; //initiate as false
+// device detection
+
 function preload(){
 
     logo = loadImage("assets/halak.png");
@@ -27,6 +32,17 @@ function preload(){
 }
 
 function setup() {
+    
+    
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+    isMobile = true;
+}
+
+    if(isMobile){
+        fishLimit = 30;
+    }
+    
     /*
     canvas = createCanvas(100,100);
     firstRun = false;
@@ -42,16 +58,18 @@ function setup() {
 
     school = new School();
 
-    for (var i = 0; i < 50; i++) {
+    for (var i = 0; i < fishLimit; i++) {
         var b = new Fish(centerPoint.x, centerPoint.y);
         school.addFish(b);
     }
-
+    
     imageMode(CENTER);
 
 }
 
 function draw() {
+
+    
     /*
     if(dimLevel <= 255){
         tint(255,dimLevel);
@@ -64,9 +82,10 @@ function draw() {
     mousePosition = createVector(mouseX, mouseY);
     positionModifier = createVector(-(mouseX - width / 2) / (width / 2), -(mouseY - height / 2) / (height / 2));
     school.run();
-    for (var i = school.schoolOfFish.length-1; i >= 0; i--) {
+    for (var i = Math.min(school.schoolOfFish.length, fishLimit)-1; i >= 0; i--) {
         school.schoolOfFish[i].render();
     }
+
     //image(logo, centerPoint.x + positionModifier.x * logoWeight, centerPoint.y + positionModifier.y * logoWeight, logo.width, logo.height);
     /*
   strokeWeight(1);
@@ -82,7 +101,7 @@ function School() {
 }
 
 School.prototype.run = function() {
-    for (var i = 0; i < this.schoolOfFish.length; i++) {
+    for (var i = 0; i < Math.min(this.schoolOfFish.length, fishLimit); i++) {
         this.schoolOfFish[i].run(this.schoolOfFish);
     }
 }
@@ -124,7 +143,7 @@ Fish.prototype.school = function(schoolOfFish) {
     var cent = this.seek(centerPoint); // Attraction to center
     var m = this.seek(mousePosition); // Attraction to mouse
 
-    
+
     var d = dist(this.position.x, this.position.y, centerPoint.x, centerPoint.y);
     var multiplier = 0;
     var force = 1.5;
@@ -143,7 +162,7 @@ Fish.prototype.school = function(schoolOfFish) {
     sep.mult(3.2);
     ali.mult(1.5);
     coh.mult(2.5);
-    
+
     this.applyForce(m);
     this.applyForce(cent);
     this.applyForce(sep);
@@ -192,7 +211,7 @@ Fish.prototype.separate = function(schoolOfFish) {
     var steer = createVector(0, 0);
     var count = 0;
 
-    for (var i = 0; i < schoolOfFish.length; i++) {
+    for (var i = 0; i < Math.min(schoolOfFish.length, fishLimit); i++) {
         var d = p5.Vector.dist(this.position, schoolOfFish[i].position);
         if ((d > 0) && (d < desiredseparation)) {
             var diff = p5.Vector.sub(this.position, schoolOfFish[i].position);
@@ -202,7 +221,7 @@ Fish.prototype.separate = function(schoolOfFish) {
             count++;
         }
     }
-    
+
     if (count > 0) {
         steer.div(count);
     }
@@ -221,7 +240,7 @@ Fish.prototype.align = function(schoolOfFish) {
     var neighbordist = 50;
     var sum = createVector(0, 0);
     var count = 0;
-    for (var i = 0; i < schoolOfFish.length; i++) {
+    for (var i = 0; i < Math.min(schoolOfFish.length, fishLimit); i++) {
         var d = p5.Vector.dist(this.position, schoolOfFish[i].position);
         if ((d > 0) && (d < neighbordist)) {
             sum.add(schoolOfFish[i].velocity);
@@ -246,7 +265,7 @@ Fish.prototype.cohesion = function(schoolOfFish) {
     var neighbordist = 50;
     var sum = createVector(0, 0);
     var count = 0;
-    for (var i = 0; i < schoolOfFish.length; i++) {
+    for (var i = 0; i < Math.min(schoolOfFish.length, fishLimit); i++) {
         var d = p5.Vector.dist(this.position, schoolOfFish[i].position);
         if ((d > 0) && (d < neighbordist)) {
             sum.add(schoolOfFish[i].position);
@@ -283,7 +302,7 @@ function canvasSetup() {
     }
     */
     if(firstRun){
-    firstRun = false;
+        firstRun = false;
         canvas = createCanvas(window.innerWidth, window.innerHeight);
     } else {
         canvas = resizeCanvas(window.innerWidth, window.innerHeight);
